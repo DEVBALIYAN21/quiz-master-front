350a7b9e7eca044006fcbd0f069ca3ef1cc2927e
Dev Baliyan <devbaliyan202@gmail.com>
2025-05-09 20:29:00 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/" element={<Dashboard />} />
             <Route path="/create" element={<CreateQuiz />} />
             <Route path="/join" element={<JoinQuiz />} />
             <Route path="/quizzes/:quizCode" element={<TakeQuiz />} />
